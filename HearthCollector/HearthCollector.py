c2ca3fc4f3e42abef85143a48a27913fb516b2f0
Eunggu Yun <yuneg11@gmail.com>
2018-12-11 23:27:20 +0900

    Add cards image source parser

--- HearthCollector/HearthCollector.py
+++ HearthCollector/HearthCollector.py
@@ -9,10 +9,17 @@ def collect_cards_list(url):
 	driver.get(url)
 	html = driver.page_source
 	source = BeautifulSoup(html, "html.parser")
-	print(source)
+
+	cards_section = source.find("div", {"id": "RevealedCards"})
+	cards_image = cards_section.find_all("img")
+
+	for card_image in cards_image:
+		card_image_src = card_image.get("data-src")
+		print(card_image_src)
 
 
-print("URL: ", end='')
-url = input()
-url = url.replace(" ", "")
+#print("URL: ", end='')
+#url = input()
+#url = url.replace(" ", "")
+url = "https://playhearthstone.com/ko-kr/expansions-adventures/rastakhans-rumble/cards"
 collect_cards_list(url)
