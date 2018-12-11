8f2d78f6542936ab89d2b5ab0346fa64ab345efb
Eunggu Yun <yuneg11@gmail.com>
2018-12-11 23:02:11 +0900

    Add url input handler and collect_cards_list function

--- HearthCollector/HearthCollector.py
+++ HearthCollector/HearthCollector.py
@@ -2,3 +2,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
 import os
+
+
+def collect_cards_list(url):
+	print(url)
+
+
+print("URL: ", end='')
+url = input()
+url = url.replace(" ", "")
+collect_cards_list(url)
